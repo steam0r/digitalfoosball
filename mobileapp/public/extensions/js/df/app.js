17e3d07aeb52d2b137abb6900fb79d8fcc1f681f
helgri <helgri@gmail.com>
2011-09-07 21:39:13 +0200

    added score check (0 < score < 10)

--- mobileapp/public/extensions/js/df/app.js
+++ mobileapp/public/extensions/js/df/app.js
@@ -43,6 +43,7 @@ df.app = (function() {
   $(".js_homeplus").live("click",function(e) {
     e.preventDefault();
     actionIfPermitted(function() {
+    ($(this).closest(".page").find("#scorehome").data("score") < 10) && actionIfPermitted(function() {
       df.publish("socket:event", "amend",{"score": "home", "goal":"plus"});
     });
   });
@@ -50,6 +51,7 @@ df.app = (function() {
   $(".js_homeminus").live("click",function(e) {
     e.preventDefault();
     actionIfPermitted(function() {
+    ($(this).closest(".page").find("#scorehome").data("score") > 0) && actionIfPermitted(function() {
       df.publish("socket:event", "amend",{"score": "home", "goal":"minus"});
     });
   });
@@ -57,6 +59,7 @@ df.app = (function() {
   $(".js_visitorsplus").live("click",function(e) {
     e.preventDefault();
     actionIfPermitted(function() {
+    ($(this).closest(".page").find("#scorevisitors").data("score") < 10) && actionIfPermitted(function() {
       df.publish("socket:event", "amend",{"score": "visitors", "goal":"plus"});
     });
   });
@@ -64,6 +67,7 @@ df.app = (function() {
   $(".js_visitorsminus").live("click",function(e) {
     e.preventDefault();
     actionIfPermitted(function() {
+    ($(this).closest(".page").find("#scorevisitors").data("score") > 0) && actionIfPermitted(function() {
       df.publish("socket:event", "amend",{"score": "visitors", "goal":"minus"});
     });
   });
