85a632958d57b4b6845a9229321534742772d23a
Stephan Maihoefer <stephan@undev.de>
2012-09-29 20:21:37 +0000

    chnge back to background process with logging updated the readme to have the current versions

--- mobileapp/README.md
+++ mobileapp/README.md
@@ -2,3 +2,14 @@
 
 See [Wiki](https://github.com/sinnerschrader/digitalfoosball/wiki/Installation-Instructions:-Part-2:-Mobile-Webapp) for installation help.
 
+This version uses, as opposed to the above link:
+  
+  NodeJs 0.6.19
+  NPM 1.1.4
+
+  socket.io 0.9.10
+  express 3.0.0rc4
+  oauth 0.9.8
+  mustache 0.7.0
+
+which should all come with raspbians apt-get repository.
--- mobileapp/lib/startup.bash
+++ mobileapp/lib/startup.bash
@@ -8,8 +8,8 @@ SERVER_BIN=$SCRIPT_PATH/server.js
 case "$1" in
     start)
     echo "Starting nodejs for mobile app..."
-    # NODE_ENV=$ENV $NODE_BIN $SERVER_BIN  >> /var/log/node_mobile_app.log 2>&1 &
-    NODE_ENV=$ENV $NODE_BIN $SERVER_BIN
+    NODE_ENV=$ENV $NODE_BIN $SERVER_BIN  >> /var/log/node_mobile_app.log 2>&1 &
+    # NODE_ENV=$ENV $NODE_BIN $SERVER_BIN
     echo "$!" > /var/run/node_mobile_app.pid
     ;;
     stop)
